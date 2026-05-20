6bc2f17a3c107d70018324df24a6685f65324473
Sahil Mansuri <sahilmansuri1552005@gmail.com>
2026-05-20 20:47:28 +0530

    first commit

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { getAllNotes, createNote, updateNote, deleteNote,getNoteById } from '../controllers/notesController.js';
+
+const router = express.Router();
+
+router.get('/', getAllNotes);
+router.get('/:id', getNoteById);
+router.post('/', createNote);
+router.put('/:id', updateNote);
+router.delete('/:id', deleteNote);
+
+
+// app.get('/api/notes', (req, res) => {
+//   res.status(200).json({ message: 'Welcome to the Notes API!' });
+// });
+
+// app.post('/api/notes', (req, res) => {
+//   res.status(201).json({ message: 'Note created successfully!' });
+// });
+
+// app.put('/api/notes/:id', (req, res) => {
+//   res.status(200).json({ message: 'Note updated successfully!' });
+// });
+
+// app.delete('/api/notes/:id', (req, res) => {
+//   res.status(200).json({ message: 'Note deleted successfully!' });
+// });
+
+export default router;
